1bdacbc929fca7c48281a983053df9cd64c9f93e
sma <samydetective@hotmail.com>
2026-01-19 18:38:49 -0800

    ultimos valores de referencia

--- js/valor-referencia.js
+++ js/valor-referencia.js
@@ -18,3 +18,18 @@ const cambiaNombre = ({...persona}) => {
 let peter = { nombre: "peter" };
 let tony = cambiaNombre(peter);
 console.log(peter, tony);
+
+
+const frutas = ["Manzana","pera","Mango"];
+//la diferencia entre dos arreglos es que uno es una referencia y el otro es una copia
+
+
+console.time("slice");
+const otrasFrutas2 = frutas.slice();
+console.timeEnd("slice");
+
+console.time("spred");
+const otrasFrutas = [...frutas ,"Banana"];
+console.timeEnd("spred");
+
+console.table({frutas,otrasFrutas,otrasFrutas2});
