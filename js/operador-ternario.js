832bd98d508450a079b02fcd3f01d1d3e7c75165
sma <samydetective@hotmail.com>
2026-01-20 17:51:21 -0800

    prop tips ternario

---
+++ js/operador-ternario.js
@@ -0,0 +1,34 @@
+
+
+const dia = 1;
+const horaActual =10;
+
+let horaApertura;
+let mensaje; //abierto o cerrado
+
+//includes verifica si un valor existe dentro del arreglo regresa true o false
+/*
+if([0,6].includes(dia) ){
+    //es fin de semana
+    console.warn("es fin de semana");
+    horaApertura = 9;
+}else {
+    console.log("dia entre semana");
+    horaApertura = 11;
+}*/
+horaApertura = ([0,6].includes(dia))? 9 :11;
+
+
+
+
+if( horaActual >= horaApertura ){
+    mensaje = "esta abierto";
+}
+else {
+    mensaje = `esta cerrado, hoy abrimos a las ${horaApertura}`;
+}
+
+console.log({
+    horaApertura,mensaje
+})
+
