7d6d6ff14bba6501d66e59bed9b9c4425b5645b6
seiku2000 <eslumter@gmail.com>
2026-01-24 05:47:18 +0000

    tip-ternario

--- js/prop-tipTernario.js
+++ js/prop-tipTernario.js
@@ -2,3 +2,7 @@ const elMayor = (a,b) =>{
     return (a >b)?a :b;
 }
 console.log(elMayor(10,15));
+
+
+const tieneMembresia = (miembre) => (miembre) ?`2 dolares`: `10 dolares`;
+console.log(tieneMembresia(false));
