2457a414db32f53aa72b1a33ababe88108b0cce4
sma <samydetective@hotmail.com>
2026-01-26 13:20:54 -0800

    swieth

---
+++ js/swieth.js
@@ -0,0 +1,32 @@
+const dia = 2;
+
+switch (dia) {
+    case 0:
+        console.log("domingo");
+        break;
+
+    case 1:
+        console.log("lunes");
+        break;
+
+    case 2:
+        console.log("martes");
+        break;
+    case 3:
+        console.log("miercoles");
+        break;
+    case 4:
+        console.log("jueves");
+        break;
+    case 5:
+        console.log("viernes");
+        break;
+    case 6:
+        console.log("sabado");
+        break;
+    default:
+        console.log("dia no valido");
+        break;
+
+
+}
