b305e05733f067ed152e011c7da4b8161d07cc4a
sma <samydetective@hotmail.com>
2026-01-26 17:30:57 -0800

    for

---
+++ js/for.js
@@ -0,0 +1,16 @@
+const heroes = ['Batman', 'Superman', 'Mujer Maravilla', 'Aquaman'];
+console.warn("for clasico");
+for (let i =0 ; i < heroes.length; i++){
+    console.log(heroes[i]);
+}
+
+console.warn("for in");
+for( let i in heroes){
+    console.log(heroes[i]);
+}
+
+console.warn("for off");
+
+for(let heroe of heroes){
+    console.log(heroe);
+}
